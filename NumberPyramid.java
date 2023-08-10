fb2114ca7a737076f69d80012fe2d034d27f8153
Shivani Lunge <118101885+shivanilunge@users.noreply.github.com>
2023-08-11 00:26:42 +0530

    Pascal's Pyramid

--- NumberPyramid.java
+++ NumberPyramid.java
@@ -1,17 +1,33 @@
-public class NumberPyramid {
-    public static void main(String[] args) {
-        int row = 5;
-         int num = 1;
-        for (int i = 1; i <= row; i++) {
-           
-            for (int j = 1; j <= row - i; j++) {
+import java.util.Scanner;
+public class NumberPyramid 
+{
+   public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows : ");
+        int rows = sc.nextInt();
+
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < rows - i; j++) 
+            {
                 System.out.print(" " + " ");
             }
-            for (int j = 1; j <= i * 2 - 1; j++) {
-
-                System.out.print(num++ + " ");
+            for (int j = 0; j <= i; j++) 
+            {
+                int num = calculatePascalValue(i, j);
+                System.out.print(" " + num + " ");
             }
             System.out.println();
+        }
+    }
+    static int calculatePascalValue(int rowNum, int colNum) 
+    {
+        if (colNum == 0 || rowNum == colNum) 
+        {
+            return 1;
+        } else {
+            return (calculatePascalValue(rowNum - 1, colNum - 1) + calculatePascalValue(rowNum - 1, colNum));
         }
     }
 }
